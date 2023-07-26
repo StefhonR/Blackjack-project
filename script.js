8ad30b44e1c95825081d41b6ad0a6c1d3ef1739d
StefhonR <stefhon100@gmail.com>
2023-07-25 21:27:26 -0400

    Writing starter code

---
+++ script.js
@@ -0,0 +1,36 @@
+const cards = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+];
+
+const cardValues = {
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10,
+    J: 10,
+    Q: 10,
+    K: 10,
+    A: 11
+};
+
+let playerCards = [];
+let dealerCards = [];
+let playerScore = 0;
+let dealerScore = 0;
